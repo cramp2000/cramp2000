2f006609e167d1a664e3863f88a3766d162881e2
Brandon Dean <70959805+cramp2000@users.noreply.github.com>
2021-08-13 18:09:44 -0400

    Add files via upload

---
+++ HelloJava.java
@@ -0,0 +1,16 @@
+package csci1302.intro;
+/**
+ * Brandon Dean
+ * CSCI 1302-D
+ * 8-13-2021
+ * A simple introductory Java class.
+ */
+public class HelloJava {
+   /**
+    * Print a silly message.
+    */
+   public static void main(String[] args) {
+       System.out.print("I am the 1337 h4x0r. Phear me!");
+   } // main(String[])
+
+} // class HelloJava
---
+++ SimpleMath.java
@@ -0,0 +1,13 @@
+package csci1302.intro;
+
+public class SimpleMath {
+	public static void main(String[] args) {
+	 int num1 = 3, num2 = 4, sum;
+	 sum = num1 + num2;
+	 System.out.println("Adding " + num1+ " and " + num2 + " gives us " + sum);
+	 
+	 
+	 
+	 
+	}
+}
